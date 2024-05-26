859406943d5a391d5eab59345119e6a235341b8f
theRate <adskiypovar@gmail.com>
2024-05-26 19:39:22 +0300

    event model

--- nafi_interactive_meetings/screencast/models.py
+++ nafi_interactive_meetings/screencast/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Event(models.Model):
+    """Сущность мероприятия"""
+    title = models.CharField('Название продукта', max_length=128, blank=False)
+    start_date = models.DateTimeField('Дата/время проведения', blank=True)
+    pdf = models.ImageField(upload_to='media/pdfs')
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Владелец')
+
+    class Meta:
+        verbose_name = 'Мероприятие'
+        verbose_name_plural = 'Мероприятия'
+
+    def __str__(self):
+        return self.title
