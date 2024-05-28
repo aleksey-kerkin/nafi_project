3571c060f1587e1f7e535ab1bfe8d658c46221f0
theRate <adskiypovar@gmail.com>
2024-05-28 19:34:43 +0300

    pdf to slides Done

--- nafi_interactive_meetings/screencast/signals.py
+++ nafi_interactive_meetings/screencast/signals.py
@@ -1,5 +1,5 @@
-from time import sleep
 from pdf2image import convert_from_path
+from uuid import uuid1
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -8,20 +8,18 @@ from django.conf import settings
 from .models import Event, Slide
 
 
-
 @receiver(post_save, sender=Event)
 def add_to_group(sender, instance, **kwargs):
-    print('!!! SIGNAL WAS WORKED !!!')
-    print('!!! SIGNAL WAS WORKED !!!')
-    print('!!! SIGNAL WAS WORKED !!!')
     # https://pypi.org/project/pdf2image/
     path_2_file = str(settings.MEDIA_ROOT) + '/' + str(instance.pdf)
-    images = convert_from_path(path_2_file, 300)  # тут нужно как-то сам файл выдернуть
-    # sleep(10)
-    for i, image in enumerate(images):
+    images = convert_from_path(path_2_file, 300)
+    for i, image in enumerate(images, 1):
+        file_name = f'user_{instance.user.pk}/{uuid1().hex}.jpg'
+        image.save(f'media/{file_name}')
         Slide.objects.create(
             title=i,
-            jpeg=image,
-            time=None,
+            time=5,
+            jpeg=file_name,
+            user=instance.user,
             event=instance
         )
