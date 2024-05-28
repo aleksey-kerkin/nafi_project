6c1ccfb67adcd5f1f40cdb57f6e329124ffb0f03
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-05-29 00:24:42 +0500

    Add QuestionDetailAPIView

--- nafi_interactive_meetings/questions/urls.py
+++ nafi_interactive_meetings/questions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import QuestionAPIView
+from .views import QuestionAPIView, QuestionDetailAPIView
 
 
 urlpatterns = [
     path("", QuestionAPIView.as_view(), name="question-list"),
+    path("<int:pk>", QuestionDetailAPIView.as_view(), name="question"),
 ]
--- nafi_interactive_meetings/questions/views.py
+++ nafi_interactive_meetings/questions/views.py
@@ -6,3 +6,8 @@ from .serializers import QuestionSerializer
 class QuestionAPIView(generics.ListCreateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+
+
+class QuestionDetailAPIView(generics.RetrieveAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
