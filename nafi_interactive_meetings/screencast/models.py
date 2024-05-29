c28785feee87f1d5d892c7e0b771009cbbed3a61
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-05-29 22:43:46 +0500

    Resolving conflicts

--- nafi_interactive_meetings/screencast/models.py
+++ nafi_interactive_meetings/screencast/models.py
@@ -6,45 +6,41 @@ from uuid import uuid1
 
 # Create your models here.
 def user_directory_path(instance, filename):
-    extension = filename.split(".")[-1]
+    extension = filename.split('.')[-1]
     name = uuid1().hex
-    filename = name + "." + extension
-    return "user_{0}/{1}".format(instance.user.id, filename)
+    filename = name + '.' + extension
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
 
 
 class Event(models.Model):
     """Сущность мероприятия"""
-
-    title = models.CharField("Название мероприятия", max_length=128, blank=False)
-    start_date = models.DateTimeField("Дата/время проведения", blank=True)
-    pdf = models.FileField("Презентация в PDF", upload_to=user_directory_path)
+    title = models.CharField('Название мероприятия', max_length=128, blank=False)
+    start_date = models.DateTimeField('Дата/время проведения', blank=True)
+    pdf = models.FileField('Презентация в PDF', upload_to=user_directory_path)
     current_slide = models.IntegerField(default=0)
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Владелец")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Владелец')
 
     class Meta:
-        verbose_name = "Мероприятие"
-        verbose_name_plural = "Мероприятия"
+        verbose_name = 'Мероприятие'
+        verbose_name_plural = 'Мероприятия'
 
     def __str__(self):
-        return f"{self.user} -> {self.title}"
+        return f'{self.user} -> {self.title}'
 
 
 class Slide(models.Model):
     """Сущность слайда/блока/темы"""
+    title = models.CharField('Название/номер слайда', max_length=128, blank=False)
+    jpeg = models.FileField('Слайд в JPEG', upload_to=user_directory_path)
+    time = models.IntegerField('Время на блок, сек')
 
-    title = models.CharField("Название/номер слайда", max_length=128, blank=False)
-    jpeg = models.FileField("Слайд в JPEG", upload_to=user_directory_path)
-    time = models.IntegerField("Время на блок, сек")
-
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Владелец")
-    event = models.ForeignKey(
-        Event, blank=False, on_delete=models.CASCADE, verbose_name="Мероприятие"
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Владелец')
+    event = models.ForeignKey(Event, blank=False, on_delete=models.CASCADE, verbose_name='Мероприятие')
 
     class Meta:
-        verbose_name = "Слайд"
-        verbose_name_plural = "Слайды"
+        verbose_name = 'Слайд'
+        verbose_name_plural = 'Слайды'
 
     def __str__(self):
-        return f"{self.event} -> {self.title}"
+        return f'{self.event} -> {self.title}'
--- nafi_interactive_meetings/screencast/urls.py
+++ nafi_interactive_meetings/screencast/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import EventAPIView, SlideAPIView
 
 urlpatterns = [
-    path("events/", EventAPIView.as_view()),
-    path("events/<int:pk>/", EventAPIView.as_view()),
-    path("slides/", SlideAPIView.as_view()),
-    path("slides/<int:pk>/", SlideAPIView.as_view()),
+    path('events/', EventAPIView.as_view()),
+    path('events/<int:pk>/', EventAPIView.as_view()),
+    path('slides/', SlideAPIView.as_view()),
+    path('slides/<int:pk>/', SlideAPIView.as_view()),
 ]
