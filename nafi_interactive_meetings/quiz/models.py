fb585c3587a809881655879d76110764ecc6bb9c
theRate <adskiypovar@gmail.com>
2024-06-01 16:20:43 +0300

    db repair

--- nafi_interactive_meetings/quiz/models.py
+++ nafi_interactive_meetings/quiz/models.py
@@ -1,49 +1,45 @@
-from django.contrib.auth.models import User
-from django.db import models
+from uuid import uuid1
 
+from django.db import models
 from django.conf import settings
+
+from screencast.models import Event
 
 
 # Create your models here.
-class Quiz(models.Model):
-    name = models.TextField('Название викторины', max_length=128)
-    description = models.TextField('Описание', max_length=512)
-    banner = models.ImageField(
-        upload_to=None,
-        height_field=600,
-        width_field=800,
-        verbose_name='Баннер викторины'
-    )
+def user_directory_path(instance, filename):
+    extension = filename.split('.')[-1]
+    name = uuid1().hex
+    filename = name + '.' + extension
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
 
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    date_c = models.DateTimeField(auto_now_add=True)
-    date_u = models.DateTimeField(auto_now=True)
+
+class Quiz(models.Model):
+    title = models.TextField('Название викторины', max_length=128)
+    description = models.TextField('Описание викторины', max_length=512)
+    banner = models.ImageField('Баннер викторины', upload_to=user_directory_path)
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Владелец')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, verbose_name='Мероприятие', blank=True, null=True)
 
     class Meta:
         verbose_name = 'Викторина'
         verbose_name_plural = 'Викторины'
 
     def __str__(self):
-        return self.name
+        return f'{self.user} -> {self.title}'
 
 
 class Question(models.Model):
-    name = models.TextField('Название вопроса', max_length=256)
-    frame = models.ImageField(
-        upload_to=None,
-        height_field=600,
-        width_field=800,
-        verbose_name='Изображение'
-    )
-    answers = {}
+    title = models.TextField('Название вопроса', max_length=256)
+    frame = models.ImageField('Изображение', upload_to=user_directory_path)
 
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    date_c = models.DateTimeField(auto_now_add=True)
-    date_u = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Владелец')
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, verbose_name='Викторина', blank=True, null=True)
 
     class Meta:
         verbose_name = 'Вопрос викторины'
         verbose_name_plural = 'Вопросы викторины'
 
     def __str__(self):
-        return self.name
+        return f'{self.quiz} -> {self.title}'
