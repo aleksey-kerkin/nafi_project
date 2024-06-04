d7efb94d3a23fa838a7391ac4d34326451322451
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-06-04 15:19:08 +0500

    Update url to access token_blacklist

--- nafi_interactive_meetings/nafi_interactive_meetings/urls.py
+++ nafi_interactive_meetings/nafi_interactive_meetings/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-# from django.views.decorators.csrf import csrf_exempt
+from rest_framework_simplejwt.views import TokenBlacklistView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,6 +10,7 @@ urlpatterns = [
     path("auth/", include("djoser.urls.jwt")),
     path("api-screencast/", include("screencast.urls")),
     path("api-questions/", include("questions.urls")),
+    path("api-token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
 ]
 
 urlpatterns += [re_path(r"^.*", TemplateView.as_view(template_name="index.html"))]
