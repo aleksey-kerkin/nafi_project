b50e4ae6011e76397e615aaff142ae3a4f880b3d
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-06-11 01:46:53 +0500

    Update Djoser settings

--- nafi_interactive_meetings/nafi_interactive_meetings/settings.py
+++ nafi_interactive_meetings/nafi_interactive_meetings/settings.py
@@ -168,13 +168,14 @@ DJOSER = {
     "SEND_CONFIRMATION_EMAIL": True,
     "SET_USERNAME_RETYPE": True,
     "SET_PASSWORD_RETYPE": True,
-    "PASSWORD_RESET_CONFIRM_URL": "password-reset/{uid}/{token}",
-    "USERNAME_RESET_CONFIRM_URL": "email-reset/{uid}/{token}",
+    "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
+    "USERNAME_RESET_CONFIRM_URL": "email/reset/confirm/{uid}/{token}",
     "ACTIVATION_URL": "activate/{uid}/{token}",
     "SEND_ACTIVATION_EMAIL": True,
     "SERIALIZERS": {
         "user_create": "auth_system.serializers.UserCreateSerializer",
         "user": "auth_system.serializers.UserCreateSerializer",
+        "current_user": "djoser.serializers.UserSerializer",
         "user_delete": "djoser.serializers.UserDeleteSerializer",
     },
     "PERMISSIONS": {
