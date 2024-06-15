c602d1f834f96767d50df94cc0984dac6efa27ea
theRate <adskiypovar@gmail.com>
2024-06-15 17:54:42 +0300

    mini update

--- nafi_interactive_meetings/quiz/serializers.py
+++ nafi_interactive_meetings/quiz/serializers.py
@@ -4,17 +4,24 @@ from .models import Quiz, Question, Answer
 
 
 class QuizSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Quiz
         fields = "__all__"
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Question
         fields = "__all__"
 
+
 class AnswerSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Answer
         fields = "__all__"
--- nafi_interactive_meetings/screencast/serializers.py
+++ nafi_interactive_meetings/screencast/serializers.py
@@ -4,12 +4,16 @@ from .models import Event, Slide
 
 
 class EventSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Event
         fields = "__all__"
 
 
 class SlideSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Slide
         fields = "__all__"
