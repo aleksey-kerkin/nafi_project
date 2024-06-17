26c29c03aea01ad056c15251c1870e5337750502
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-06-18 00:44:48 +0500

    Update permissions

--- nafi_interactive_meetings/screencast/views.py
+++ nafi_interactive_meetings/screencast/views.py
@@ -4,21 +4,31 @@ from .serializers import EventSerializer, SlideSerializer
 
 
 class EventViewSet(viewsets.ModelViewSet):
-    queryset = Event.objects.all()  # почему-то ругается без него, хотя по инструкции должно без него работать
+    queryset = (
+        Event.objects.all()
+    )  # почему-то ругается без него, хотя по инструкции должно без него работать
     serializer_class = EventSerializer
 
     def get_queryset(self):
         user = self.request.user
-        return Event.objects.filter(user=user)
+        if user.id is None:
+            return None
+        else:
+            return Event.objects.filter(user=user)
 
 
 class SlideViewSet(viewsets.ModelViewSet):
-    queryset = Slide.objects.all()  # почему-то ругается без него, хотя по инструкции должно без него работать
+    queryset = (
+        Slide.objects.all()
+    )  # почему-то ругается без него, хотя по инструкции должно без него работать
     serializer_class = SlideSerializer
 
     def get_queryset(self):
         user = self.request.user
-        return Slide.objects.filter(user=user)
+        if user.id is None:
+            return None
+        else:
+            return Slide.objects.filter(user=user)
 
 
 class SlideListByEvent(generics.ListAPIView):
@@ -26,5 +36,9 @@ class SlideListByEvent(generics.ListAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        event_id = self.kwargs['event']
-        return Slide.objects.filter(user=user, event=event_id)
+        if user.id is None:
+            event_id = self.kwargs["event"]
+            return Slide.objects.filter(event=event_id)
+        else:
+            event_id = self.kwargs["event"]
+            return Slide.objects.filter(user=user, event=event_id)
